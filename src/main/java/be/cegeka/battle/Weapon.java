2fc7474d79b11c9fc7e091a489e3e4aab2a55c4d
Programmer <Lindert_vdp@hotmail.com>
2018-05-24 11:31:52 +0200

    Wapensklassen aangemaakt

---
+++ src/main/java/be/cegeka/battle/Weapon.java
@@ -0,0 +1,15 @@
+package be.cegeka.battle;
+
+
+public abstract class Weapon {
+
+    private int damage;
+
+    public Weapon() {
+
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
